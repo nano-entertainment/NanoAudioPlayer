c920c01438207ed825436bb96cf26c21badf055d
Lasse Nielsen <lasse.nielsen@me.com>
2013-02-03 17:49:23 +0100

    Add defaultoptions and event listener

--- jquery.nanoAudioPlayer.js
+++ jquery.nanoAudioPlayer.js
@@ -6,7 +6,30 @@
  * http://github.com/nano-entertainment/NanoAudioPlayer
  */
 (function($){
-  $.fn.nanoAudioPlayer = function(){
+  $.fn.nanoAudioPlayer = function( options ){
+    var defaults = {
+      autoplay: false,
+      element: new Audio()
+    };
+    
+    var opts = $.extend(defaults, options);
+    
+    // EventListener autoplay / loop
+    
+    opts.element.bind('canplay', function() {
+      if( opts.autoplay ) opts.element.play();
+    });
+    
+    
+    // Html-Event Listener
+    
+    $(this).find('.play-toggle').bind('click', function() {
+      if(opts.audioElement.paused) {
+    	opts.audioElement.play();
+      } else {
+        opts.audioElement.pause();
+      }
+    });
 
   }
 })(jQuery);
